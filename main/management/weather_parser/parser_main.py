a82f9d48874edf2a0956f197989f7fa6911fef85
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2022-01-04 15:25:46 +0300

    Change static_root folder, took out config reading

--- main/management/weather_parser/parser_main.py
+++ main/management/weather_parser/parser_main.py
@@ -1,41 +1,29 @@
 from datetime import date, datetime, timedelta
 from os import listdir, mkdir, path, remove
-
 from requests import Session, Response
 from random import randint
 from time import sleep
 import zlib
-import configparser
 from collections import deque
 from psycopg2 import Error
-
 from main.management.weather_parser.classes import WeatherStation
 from main.management.weather_parser.db import db
 import main.management.weather_parser.rp5_parser as rp5_parser
 import main.management.weather_parser.processing as processing
 import main.management.weather_parser.weather_csv as weather_csv
 import main.management.weather_parser.queries as queries
+from weather.settings import App, WEATHER_PARSER, STATIC_ROOT
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-DELIMITER = config["csv"]["delimiter"]
-STATIC_ROOT = config["path"]["static_root"]
-SAVE_IN_DB = False if config["db"]["database"] == '' else True
-# Replace in config every time when you need update your sources
-yandex = {
-    "login": config["yandex"]["login"],
-    "pass": config["yandex"]["pass"],
-    "api_key": config["yandex"]["api_key"],
-    "token": config["yandex"]["token"],
-    "id": config["yandex"]["id"],
-}
 
+SAVE_IN_DB = False if App.database.name == '' else True
+DELIMITER = WEATHER_PARSER['CSV_DELIMITER']
+_STATIC_ROOT = STATIC_ROOT / 'csv_data'
 current_session: Session
 
 
 def create_directory(ws: WeatherStation):
     try:
-        mkdir(rf"{STATIC_ROOT}{ws.number}")
+        mkdir(rf"{_STATIC_ROOT}{ws.number}")
     except OSError as e:
         # 17 - FileExistsError, folder was created earlier.
         if e.errno != 17:
@@ -75,7 +63,7 @@ def get_weather_for_year(start_date: date, number: str, ws_id: int, url: str, da
 
         download_link = rp5_parser.get_link_archive_file(answer.text)
 
-        with open(f'{STATIC_ROOT}{number}/{start_date.year}.csv', "wb") as file:
+        with open(f'{_STATIC_ROOT}{number}/{start_date.year}.csv', "wb") as file:
             response = current_session.get(download_link)
             while response.status_code != 200:
                 response: Response = current_session.get(download_link)
@@ -104,21 +92,21 @@ def get_weather_for_year(start_date: date, number: str, ws_id: int, url: str, da
 
 
 def load_data_from_csv(folder: str, data_type: int):
-    global STATIC_ROOT
+    global _STATIC_ROOT
 
-    if path.isdir(f"{STATIC_ROOT}{folder}"):
-        for weather_file in listdir(f"{STATIC_ROOT}{folder}"):
-            if path.isfile(f"{STATIC_ROOT}{folder}\\{weather_file}") and weather_file[-4:] == '.csv':
+    if path.isdir(f"{_STATIC_ROOT}{folder}"):
+        for weather_file in listdir(f"{_STATIC_ROOT}{folder}"):
+            if path.isfile(f"{_STATIC_ROOT}{folder}\\{weather_file}") and weather_file[-4:] == '.csv':
                 try:
                     if data_type == 0:
                         # TODO: Отказаться от использования pyDAL использовать встроенное в Django
                         db.executesql(queries.insert_csv_weather_station_data(
-                            f"{STATIC_ROOT}{folder}\\{weather_file}",
+                            f"{_STATIC_ROOT}{folder}\\{weather_file}",
                             DELIMITER))
                     elif data_type == 1:
                         # TODO: Отказаться от использования pyDAL использовать встроенное в Django
                         db.executesql(queries.insert_csv_metar_data(
-                            f"{STATIC_ROOT}{folder}\\{weather_file}",
+                            f"{_STATIC_ROOT}{folder}\\{weather_file}",
                             DELIMITER))
                     db.commit()
                 except Error as e:
@@ -128,18 +116,18 @@ def load_data_from_csv(folder: str, data_type: int):
                         print(f"My error: {e.pgcode}. File in folder {folder}\\{weather_file}.")
                         raise
                     else:
-                        remove(f"{STATIC_ROOT}{folder}\\{weather_file}")
+                        remove(f"{_STATIC_ROOT}{folder}\\{weather_file}")
                 else:
-                    remove(f"{STATIC_ROOT}{folder}\\{weather_file}")
+                    remove(f"{_STATIC_ROOT}{folder}\\{weather_file}")
 
 
 def get_all_data() -> list[WeatherStation]:
     """ Function get all weather data for all weather stations from csv file
         from start date of observations to today or update data from date of last
         getting weather."""
-    global current_session, yandex
+    global current_session
     links_with_404: list[WeatherStation] = list()
-    wanted_stations = weather_csv.read_csv_file(STATIC_ROOT, DELIMITER)
+    wanted_stations = weather_csv.read_csv_file(_STATIC_ROOT, DELIMITER)
 
     indexes_of_duplicates = list()
     if wanted_stations:
@@ -152,7 +140,7 @@ def get_all_data() -> list[WeatherStation]:
             url = station.link[0:14]
 
             if station.start_date is None or station.number is None:
-                status, station = rp5_parser.get_missing_ws_info(current_session, SAVE_IN_DB, station, yandex)
+                status, station = rp5_parser.get_missing_ws_info(current_session, SAVE_IN_DB, station, App.yandex)
                 print(f"Start getting data for {station.place} with "
                       f"start date of observations {station.start_date}...")
 
@@ -214,7 +202,7 @@ def get_all_data() -> list[WeatherStation]:
             if flag:
                 if SAVE_IN_DB:
                     load_data_from_csv(station.number, station.data_type)
-                weather_csv.update_csv_file(STATIC_ROOT, DELIMITER, station, index)
+                weather_csv.update_csv_file(_STATIC_ROOT, DELIMITER, station, index)
                 # Use timeout between sessions for concealment
                 sleep(randint(3, 10))
                 print("Data was loaded!")
@@ -224,18 +212,18 @@ def get_all_data() -> list[WeatherStation]:
 
 def create_csv_by_country(url) -> None:
     """Function find all places, links and types (SYNOP, METAR, weather sensors) on site rp5.ru for country."""
-    global STATIC_ROOT, DELIMITER
+    global _STATIC_ROOT, DELIMITER
 
     pages = deque([url])
     # TODO: another variable isn't used. Она нужна чтобы посмотреть на ссылки, которые не были добавлены
     links, another = rp5_parser.get_pages_with_weather_at_place(pages)
 
-    with open(f"{STATIC_ROOT}links.txt", "w", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}links.txt", "w", encoding="utf-8") as file:
         for link in links:
             file.write("%s\n" % link)
 
     links, lines = [], []
-    with open(f"{STATIC_ROOT}links.txt", "r", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}links.txt", "r", encoding="utf-8") as file:
         lines = file.readlines()
 
     for line in lines:
@@ -244,30 +232,30 @@ def create_csv_by_country(url) -> None:
     # Получаем list[list[str - название места, str - url, int - тип]] ,
     # через каждые X ссылок начинаем новую сессию
     # TODO: вынести логику создания новой сессии в отдельную функцию
-    rp5_parser.get_link_type(links, STATIC_ROOT, DELIMITER)
+    rp5_parser.get_link_type(links, _STATIC_ROOT, DELIMITER)
 
     # If you start find_sources command from the middle
     delete_not_unique_elements()
 
 
 def delete_not_unique_elements() -> None:
-    with open(f"{STATIC_ROOT}places.txt", "r", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}places.txt", "r", encoding="utf-8") as file:
         lines = file.readlines()
         lines = list(set(lines))
 
-    with open(f"{STATIC_ROOT}places.txt", "w", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}places.txt", "w", encoding="utf-8") as file:
         for line in lines:
             file.write(line)
 
 
 def update_sources() -> None:
-    with open(f"{STATIC_ROOT}places.txt", "r", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}places.txt", "r", encoding="utf-8") as file:
         sources: list = file.readlines()
     for i, source in enumerate(sources):
         sources[i] = source.split(DELIMITER)
         sources[i] = [sources[i][0], sources[i][1].replace('http', 'https'), sources[i][2]]
 
-    with open(f"{STATIC_ROOT}cities.txt", "r", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}cities.txt", "r", encoding="utf-8") as file:
         lines: list = file.readlines()
     for i, line in enumerate(lines):
         lines[i] = line.split(DELIMITER)
@@ -285,7 +273,7 @@ def update_sources() -> None:
     # print(len(sources))
     # print(f"{count=}")
 
-    with open(f"{STATIC_ROOT}cities.txt", "a+", encoding="utf-8") as file:
+    with open(f"{_STATIC_ROOT}cities.txt", "a+", encoding="utf-8") as file:
         file.write('\n')
         for index, source in enumerate(sources):
             # Adding weather stations and metar
